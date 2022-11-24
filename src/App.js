5531f6e686ea71c2f3fb1cfd64964e1b5e3711bb
raj073 <rajoni.das@outlook.com>
2022-11-24 11:10:43 +0600

    Router and navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes/Routes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+
   return (
-    <div className="App">
+
+
+    <div className="max-w-[1200px] mx-auto">
+
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
       
     </div>
+
+
   );
 }
 
