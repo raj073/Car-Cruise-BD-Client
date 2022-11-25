da2a9cd9046f01cd11e776df6f552e2cb010f10e
raj073 <rajoni.das@outlook.com>
2022-11-25 17:32:10 +0600

    Private Route Added

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider';
+import Loading from '../../Pages/Shared/Loading/Loading';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({children}) => {
 
-    return (
-        
-        <div>
-            
-        </div>
-    );
+    const {user,loading} = useContext(AuthContext);
+    const location = useLocation();
+
+    if(loading){
+        <Loading></Loading>
+    }
+
+    if(user){
+        return children;
+    }
+
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>
+
 };
 
 export default PrivateRoute;
